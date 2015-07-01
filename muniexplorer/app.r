7ed1e49ab0c3712f96e1fd1dee3ae008bb5788f5
Charles H. Thompson <hans.thompson1@gmail.com>
2015-06-30 17:42:38 -0800

    add working leaflet app

--- muniexplorer/app.r
+++ muniexplorer/app.r
@@ -1,12 +1,22 @@
 library(shiny)
 library(leaflet)
+load("data/all_the_data.rda")
+
+precincts <- levels(factor(all_the_data$NAME))
+
+#input <- list(select = "Spenard")
+
+
+
+head(all_the_data)
 
 ui <- bootstrapPage(
     tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
     leafletOutput("map", width = "100%", height = "100%"),
     absolutePanel(top = 10, right = 10,
-                  sliderInput("range", "Magnitudes", min(quakes$mag), max(quakes$mag),
-                              value = range(quakes$mag), step = 0.1
+                  selectInput("select", label = h3("Select box"), 
+                              choices = precincts, 
+                              selected = "Spenard")
                   )
     )
 )
@@ -15,21 +25,16 @@ server <- function(input, output, session) {
     
     # Reactive expression for the data subsetted to what the user selected
     filteredData <- reactive({
-        quakes[quakes$mag >= input$range[1] & quakes$mag <= input$range[2],]
+         all_the_data %>% filter(as.character(NAME) == input$select) %>% select(x, y) %>%
+            rename(lng = x, lat = y)
     })
-    
-    # This reactive expression represents the palette function,
-    # which changes as the user makes selections in UI.
-    colorpal <- reactive({
-        colorNumeric(input$colors, quakes$mag)
-    })
+
     
     output$map <- renderLeaflet({
         # Use leaflet() here, and only include aspects of the map that
         # won't need to change dynamically (at least, not unless the
         # entire map is being torn down and recreated).
-        leaflet(quakes) %>% addTiles() %>%
-            fitBounds(~min(long), ~min(lat), ~max(long), ~max(lat))
+        leaflet(data = filteredData()) %>% addTiles() %>% addProviderTiles("Stamen.Toner") %>% addMarkers() 
     })
     
     # Incremental changes to the map (in this case, replacing the
@@ -38,20 +43,9 @@ server <- function(input, output, session) {
     # should be managed in its own observer.
     observe({
         
-        leafletProxy("map", data = filteredData()) %>%
-            clearShapes() %>%
-            addCircles(radius = ~10^mag/10, weight = 1, color = "#777777", fillOpacity = 0.7, popup = ~paste(mag)
-            )
+        leafletProxy("map", data = filteredData()) 
     })
     
-    # Use a separate observer to recreate the legend as needed.
-    observe({
-        proxy <- leafletProxy("map", data = quakes)
-        
-        # Remove any existing legend, and only if the legend is
-        # enabled, create a new one.
-        proxy 
-    })
 }
-
+1
 shinyApp(ui, server)
