69f3b5de7337917a339ee7fae16d2df903ba585e
Charles H. Thompson <hans.thompson1@gmail.com>
2015-07-08 22:28:48 -0800

    made changes.

--- kenaisockeye/server.r
+++ kenaisockeye/server.r
@@ -5,8 +5,7 @@ library(lubridate)
 library(stringr)
 library(gridExtra)
 library(RCurl)
-
-
+library(scales)
 
 
 shinyServer(function(input, output) {  
@@ -29,6 +28,7 @@ shinyServer(function(input, output) {
       
   sonar_reactive <- reactive({
     year(sonar$date) <- 2014
+    factor(sonar$year)
     start_year <- input$year[1]
     end_year   <- input$year[2]
     start_date <- ymd(paste("2014", input$start_date, sep = "-"))
@@ -37,8 +37,8 @@ shinyServer(function(input, output) {
     sonar %>% 
       filter(year >= start_year, year <= end_year, 
              date >= start_date,
-             date <= end_date
-  )
+             date <= end_date) %>%
+      mutate(year = factor(year))
     
   #sonar_reactive <- sonar
   })
@@ -53,34 +53,13 @@ shinyServer(function(input, output) {
 
   #######PLOTING#########
   prior_barchart <- reactive({
-    p <- ggplot(data = sonar_reactive(), aes(x = date, y = n, group = year, fill = factor(year))) + 
-         geom_bar(stat = "identity") + ylab("Daily Count") + xlab("Date")
+    p <- ggplot(data = sonar_reactive(), aes(x = date, y = n, group = year, fill = year)) + 
+         geom_bar(stat = "identity") + ylab("Daily Count") + xlab("Date") +
+      scale_y_continuous(labels =comma)
     return(p)    
   })
-  
-  
-  testFishReactive <- reactive({
-    p <- ggplot(data = testFish, aes(x = Date, y = StationCount, color = allIDs, fill = allIDs)) +
-           geom_bar(stat = "identity") +
-           ggtitle("Total Test Fish Caught in Cook Inlet") +
-           ylab("Total Fish") +
-           annotate("text",
-                    x=unique(testFish$Date[testFish$Comment != ""]),
-                    y=1,
-                    label="*", size = 8) + 
-           theme(legend.title=element_blank())
-  
-    return(p) 
-  })
-
-    #########################
 
   ######Publishing#########
-
-  ###TABSET: REAL TIME#####
-  #output$realtime <- renderPlot({
-  #  print(realtime_chart())
-  #})
 
   output$realtime <- renderPlot({
   x <- getURL("http://www.adfg.alaska.gov/sf/FishCounts/index.cfm?ADFG=export.excel&countLocationID=40&year=2015&speciesID=420")
@@ -95,19 +74,11 @@ shinyServer(function(input, output) {
      geom_bar(data = this_season, aes(x = count_date, y = fish_count), fill = "red", stat = "identity") + 
      ylab("Fish Count") + xlab("Date in 2015") + ggtitle("Kenai River Sockeye")  + 
      geom_vline(xintercept = as.numeric(ymd("2015-07-10")), linetype = "dashed", colour="#000099") +
-     geom_vline(xintercept = as.numeric(ymd("2015-07-31")), linetype = "dashed", colour="#000099")
-      
-  })
+     geom_vline(xintercept = as.numeric(ymd("2015-07-31")), linetype = "dashed", colour="#000099") +
+    scale_y_continuous(labels =comma)
   
+  })
 
-  #output$testFishery <- renderPlot({
-  #  print(testFishReactive())
-  #})
-
-  #output$testFisheryComments <- renderPlot({
-  #  grid.table(d = unique(testFish[testFish$Comment != "", c(2, 9)]),
-  #             show.rownames = FALSE)
-  #})
   
   ###TABSET: PRIOR DATA####
   output$linechart <- renderPlot({
--- kenaisockeye/ui.R
+++ kenaisockeye/ui.R
@@ -4,41 +4,29 @@ library(ggplot2)
 shinyUI(fluidPage(theme = "bootstrap.css",
   
   # Application title
-  headerPanel("Dipnet_App! - IN BETA"),
+  #headerPanel(),
   
-  # Sidebar with a slider input for number of observations
-  sidebarPanel(
-  
-  wellPanel(
-    #helpText("Hi. Play around with the inputs to subset the historical data or look at the predictions."),
-    sliderInput("year", "Years of Interest", min = 1979, max = 2012, value = c(2002, 2012), sep = ""),
-    textInput("start_date", "Start Date:", "06-30"),
-    textInput("end_date", "End Date:", "08-05")
-  ),
-  wellPanel(   
-    img(src = "codeforanc.png"),
-    helpText("")
-    ),
-  wellPanel(
-      helpText(HTML("<b>VERSION CONTROL</b>")),
-      HTML('Version 0.9.0'),
-      HTML('<br>'),
-      HTML('Deployed on June 3rd, 2014'),
-      HTML('<br>'),
-      HTML('<a href="https://github.com/hansthompson/shiny-server/tree/master/kenaisockeye" target="_blank">Code on GitHub</a>')
-    ) 
-
-  ),
-  
-  mainPanel(
-    
+  navbarPage(title = "Dipnet_App! - IN BETA",
     tabsetPanel(
-      tabPanel("2015 Real Time", 
+      tabPanel("2015 Real Time",
                plotOutput("realtime"),
-               plotOutput("testFishery"),
-               plotOutput("testFisheryComments")
-      ), 
-      tabPanel("Prior Sonar Counts", 
+               fluidRow(   
+                 column(width = 3, a( img(src = "codeforanc.png"), href = "http://codeforanchorage.org/")),
+                 column(width = 6, offset = 3, wellPanel(helpText(""),
+                 helpText(HTML("<b>VERSION CONTROL</b>")),
+                 HTML('Version 0.9.5'),
+                 HTML('<br>'),
+                 HTML('Deployed on June 3rd, 2014'),
+                 HTML('<br>'),
+                 HTML('<a href="https://github.com/hansthompson/shiny-server/tree/master/kenaisockeye" target="_blank">Code on GitHub</a>')
+               )))), 
+      tabPanel("Prior Sonar Counts",
+               wellPanel(
+                 #helpText("Hi. Play around with the inputs to subset the historical data or look at the predictions."),
+                 sliderInput("year", "Years of Interest", min = 1979, max = 2012, value = c(2002, 2012), sep = ""),
+                 textInput("start_date", "Start Date:", "06-30"),
+                 textInput("end_date", "End Date:", "08-05")
+               ),
                plotOutput("barchart")),
       tabPanel("About", includeMarkdown("docs/introduction.md"))
                
