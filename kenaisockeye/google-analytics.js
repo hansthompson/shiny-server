5b0e59a19d3ae91351b1fc5e8eaa34bd987cdb99
Charles Hans Thompson <hans.thompson1@gmail.com>
2015-07-16 09:03:48 -0800

    add google analytics

--- kenaisockeye/google-analytics.js
+++ kenaisockeye/google-analytics.js
@@ -3,5 +3,6 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-59056107-2', 'auto');
+  ga('create', 'UA-65211999-1', 'auto');
   ga('send', 'pageview');
+
