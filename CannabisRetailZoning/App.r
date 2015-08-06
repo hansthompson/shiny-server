4b120627580828f1596e1ef029ded139a9513e10
Charles Hans Thompson <hans.thompson1@gmail.com>
2015-08-06 13:49:08 -0800

    add test app for device orientation

--- CannabisRetailZoning/App.r
+++ CannabisRetailZoning/App.r
@@ -5,22 +5,23 @@ library(geojsonio)
 load("map.rda") 
 
 ui <- bootstrapPage(title = "Cannabis Cafe Zoning",
-    tags$head(includeScript("google-analytics.js")),
-    tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
-    leafletOutput("map", width = "100%", height = "100%"),
-    absolutePanel(class = "panel panel-default",top = 10, right = 10, width = 330,
-                  p("Since several successful initiaitves in serveral states to legalize the retail sale of Cannabis, local communities have been proposing and enacting zoning laws distancing new stores from schools and daycares. This interactive map of Anchorage allows you to simulate the effect of this kind of zoning law."),  
-                  p("Using publicly available parcel data from 2014 for over 200 zoned schools and daycares you can visualize the effect that different distance buffers would have on where Anchorage's first marijuana cafes could be."),                  
-                  numericInput("feetbuffer", label = h4("Buffer From Parcel (ft.)"), 1000)
-                  )
-    )
+                    tags$head(includeScript("google-analytics.js")),
+                    tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
+                    leafletOutput("map", width = "100%", height = "100%"),
+                    absolutePanel(class = "panel panel-default",top = 10, right = 10, width = 330,
+                                  p("Since several successful initiaitves in serveral states to legalize the retail sale of Cannabis, local communities have been proposing and enacting zoning laws distancing new stores from schools and daycares. This interactive map of Anchorage allows you to simulate the effect of this kind of zoning law."),  
+                                  p("Using publicly available parcel data from 2014 for over 200 zoned schools and daycares you can visualize the effect that different distance buffers would have on where Anchorage's first marijuana cafes could be."),                  
+                                  numericInput("feetbuffer", label = h4("Buffer From Parcel (ft.)"), 1000),
+                                  actionButton("updateButton", "Update")
+                    )
+)
 
 server <- function(input, output, session) {
-  filteredData <- reactive({
+  filteredData <- eventReactive(input$updateButton, {
     spTransform(map, CRS("+init=epsg:26934")) %>%  
-     gBuffer(width = input$feetbuffer / 3.28084) %>%
-     spTransform(CRS("+proj=longlat")) %>%
-     as("SpatialPolygonsDataFrame")
+      gBuffer(width = input$feetbuffer / 3.28084) %>%
+      spTransform(CRS("+proj=longlat")) %>%
+      as("SpatialPolygonsDataFrame")
   })
   output$map <- renderLeaflet({
     leaflet() %>% addTiles(urlTemplate = "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png") %>% setView(-149.85, 61.15, zoom = 12) %>%
@@ -30,5 +31,5 @@ server <- function(input, output, session) {
     leafletProxy("map", data = filteredData()) 
   })
 }
-  
+
 shinyApp(ui, server)
---
+++ test/ui.R
@@ -0,0 +1,22 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+shinyUI(fluidPage(
+
+    tags$head(tags$script(src="trackorientation.js")),
+
+  # Application title
+  titlePanel("Old Faithful Geyser Data"),
+
+
+    # Show a plot of the generated distribution
+    mainPanel(textOutput("text")
+
+  )
+))
