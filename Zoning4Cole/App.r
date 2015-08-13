2655d0317c99758c0bdb837e4f27c6492483d953
Charles H. Thompson <hans.thompson1@gmail.com>
2015-08-12 23:54:07 -0800

    add stuff

--- Zoning4Cole/App.r
+++ Zoning4Cole/App.r
@@ -1,18 +1,21 @@
+library(shinythemes)
 library(rgeos)
 library(rgdal)
 library(leaflet)
 library(geojsonio)
 load("map.rda") 
 
-ui <- bootstrapPage(title = "Cannabis Cafe Zoning",
+ui <- bootstrapPage(theme = shinytheme("Spacelab"),
+                    title = "Cannabis Cafe Zoning",
                     tags$head(includeScript("google-analytics.js")),
                     tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
                     leafletOutput("map", width = "100%", height = "100%"),
                     absolutePanel(class = "panel panel-default",top = 10, right = 10, width = 330,
-                                  p("Since several successful initiaitves in serveral states to legalize the retail sale of Cannabis, local communities have been proposing and enacting zoning laws distancing new stores from schools and daycares. This interactive map of Anchorage allows you to simulate the effect of this kind of zoning law."),  
-                                  p("Using publicly available parcel data from 2014 for over 200 zoned schools and daycares you can visualize the effect that different distance buffers would have on where Anchorage's first marijuana cafes could be."),                  
-                                  numericInput("feetbuffer", label = h4("Buffer From Parcel (ft.)"), 1000),
-                                  actionButton("updateButton", "Update")
+                                  includeMarkdown("docs/about.md"),
+                                  numericInput("feetbuffer", label = h4("Feet From Facility"), 500),
+                                  actionButton("updateButton", "Update"),
+                                  br(),
+                                  a(img(src = "codeforanc.png"), href = "http://codeforanchorage.org/"))
                     )
 )
 
---
+++ Zoning4Cole/docs/about.md
@@ -0,0 +1,3 @@
+### Abiding by the [Cole Memo](https://en.wikipedia.org/wiki/James_M._Cole#August.2C_2013_.22Cole_Memo.22):
+
+To address federal concerns in Alaska, the Marijuana Control Board is considering a buffer around schools and child-care facilities where new businesses for production, processing, and sale could not operate. Different zone sizes have been proposed but you can see the effects for Anchorage right here for yourself! 
