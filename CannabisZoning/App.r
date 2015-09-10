3af6e8aadfa2bea88f8c0f67fecc1e9e4a51cb92
Charles H. Thompson <hans.thompson1@gmail.com>
2015-09-09 16:49:17 -0800

    add zoning

--- CannabisZoning/App.r
+++ CannabisZoning/App.r
@@ -3,7 +3,10 @@ library(rgeos)
 library(rgdal)
 library(leaflet)
 library(geojsonio)
+
 load("map.rda") 
+load("zones.rda")
+zones_filtered <- zones
 
 ui <- bootstrapPage(theme = shinytheme("spacelab"),
                     title = "Cannabis Business Zoning",
@@ -13,6 +16,7 @@ ui <- bootstrapPage(theme = shinytheme("spacelab"),
                     absolutePanel(class = "panel panel-default",top = 10, right = 10, width = 330,
                                   includeMarkdown("docs/about.md"),
                                   numericInput("feetbuffer", label = h4("Feet From Facility"), 500),
+                                  selectInput(zones, "Specific Zone", zones@data, as.character(levels(zones@data$ZONING_DES)))
                                   actionButton("updateButton", "Update"),
                                   br(),br(),
                                   a(img(src = "codeforanc.png"), href = "http://codeforanchorage.org/"),
@@ -23,15 +27,20 @@ ui <- bootstrapPage(theme = shinytheme("spacelab"),
 
 
 server <- function(input, output, session) {
-  filteredData <- eventReactive(input$updateButton, {
+  buffers <- eventReactive(input$updateButton, {
     spTransform(map, CRS("+init=epsg:26934")) %>%  
-      gBuffer(width = input$feetbuffer / 3.28084) %>%
+      gBuffer(width = inpu111t$feetbuffer / 3.28084) %>%
       spTransform(CRS("+proj=longlat")) %>%
       as("SpatialPolygonsDataFrame")
   })
+  filteredZones <- eventReactive(input$updateButton, {
+    zones_filtered@data <- zones@data[zones@data == input$zone,]
+    return(zones_filtered)
+  })
+  
   output$map <- renderLeaflet({
     leaflet() %>% addTiles(urlTemplate = "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png") %>% setView(-149.85, 61.15, zoom = 12) %>%
-      addGeoJSON(geojson_json((filteredData())))
+      addGeoJSON(geojson_json((buffers()))) + addGeoJSON(filteredZones())
   })
   observe({
     leafletProxy("map", data = filteredData()) 
