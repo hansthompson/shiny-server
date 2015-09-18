556a44283ce41b6b60b8b050535dc1452fb29f3a
Charles H. Thompson <hans.thompson1@gmail.com>
2015-09-18 10:42:54 -0800

    add zoning and fix initial loading

--- CannabisZoning/App.r
+++ CannabisZoning/App.r
@@ -1,3 +1,5 @@
+##Need to combine Geojson into one file before putting into leaflet
+
 library(shinythemes)
 library(rgeos)
 library(rgdal)
@@ -5,46 +7,51 @@ library(leaflet)
 library(geojsonio)
 
 load("map.rda") 
-load("zones.rda")
-zones_filtered <- zones
+#load("zones.rda")
+#zones_filtered <- zones
+
+#input <- list(feetbuffer = 500, zones = "B-3")
 
 ui <- bootstrapPage(theme = shinytheme("spacelab"),
                     title = "Cannabis Business Zoning",
                     tags$head(includeScript("google-analytics.js")),
                     tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
                     leafletOutput("map", width = "100%", height = "100%"),
-                    absolutePanel(class = "panel panel-default",top = 10, right = 10, width = 330,
+                    absolutePanel(class = "panel panel-default", 
+                                  top = 10, right = 10, width = 330,
                                   includeMarkdown("docs/about.md"),
-                                  numericInput("feetbuffer", label = h4("Feet From Facility"), 500),
-                                  selectInput("zones", "Specific Zone", as.character(levels(zones@data$ZONING_DES))),
-                                  actionButton("updateButton", "Update"),
+                                  numericInput("feetbuffer", 
+                                               label = h4("Feet From Facility"), 500),
+                                  #selectInput("zones", "Specific Zone", as.character(levels(zones@data$ZONING_DES))),
+                                  actionButton("recalc", "Update"),
                                   br(),br(),
-                                  a(img(src = "codeforanc.png"), href = "http://codeforanchorage.org/"),
+                                  a(img(src = "codeforanc.png"), 
+                                    href = "http://codeforanchorage.org/"),
                                   br(),br(),
                                   (a("Contact", href = "mailto:hans.thompson1@gmail.com"))
                                   )
                 )
 
-
 server <- function(input, output, session) {
-  buffers <- eventReactive(input$updateButton, {
-    spTransform(map, CRS("+init=epsg:26934")) %>%  
-      gBuffer(width = input$feetbuffer / 3.28084) %>%
+  buffer <- eventReactive(input$recalc, {
+      spTransform(map, CRS("+init=epsg:26934")) %>%  
+      gBuffer(width = input$feetbuffer / 3.28084) %>% #input$feetbuffer is actually in meters and is converted to feet in this line
       spTransform(CRS("+proj=longlat")) %>%
       as("SpatialPolygonsDataFrame")
-  })
-  filteredZones <- eventReactive(input$updateButton, {
-    zones_filtered@data <- zones@data[zones@data == input$zone,]
-    return(zones_filtered)
-  })
+  }, ignoreNULL = FALSE)
+  #filteredZones <- eventReactive(input$updateButton, {
+  #zones_filtered@data <- zones@data[as.character(zones@data$ZONING_DES) == input$zone,] 
+  #zones_filtered <-     as(zones_filtered, "SpatialPolygonsDataFrame")
+  #})
   
   output$map <- renderLeaflet({
-    leaflet() %>% addTiles(urlTemplate = "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png") %>% setView(-149.85, 61.15, zoom = 12) %>%
-      addGeoJSON(geojson_json((buffers()))) + addGeoJSON(geojson_json(filteredZones()))
+    #SpatialPolygons(list(buffers, zones), c("buffer", "zones"))
+    leaflet() %>% 
+      addTiles(urlTemplate = "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png") %>% 
+      setView(-149.85, 61.15, zoom = 12) %>%
+      addGeoJSON(geojson_json(buffer())) #+ addGeoJSON(geojson_json(filteredZones))
   })
-  observe({
-    leafletProxy("map", data = buffers()) 
-  })
+
 }
 
 shinyApp(ui, server)
